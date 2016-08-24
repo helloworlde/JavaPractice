54be8655b75a379e65a1238d91a13bbc7824a719
helloworlde <501856639@qq.com>
2016-08-24 20:50:14 +0800

    - 添加反射和代理

---
+++ ReflectAndLoad/src/createobj/CreateJFrame.java
@@ -0,0 +1,7 @@
+package createobj;
+
+/**
+ * Created by alpha on 16-8-24.
+ */
+public class CreateJFrame {
+}
---
+++ ReflectAndLoad/src/proxytest/ProxyTest.java
@@ -0,0 +1,7 @@
+package proxytest;
+
+/**
+ * Created by alpha on 16-8-24.
+ */
+public class ProxyTest {
+}
